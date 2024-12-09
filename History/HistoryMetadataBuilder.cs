5ffb9bac85b8bdf6da5a9df7ee93f763b621b63c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-09 21:57:10 +0100

    Add HistoryMetadataBuilder class for message/item metadata
    Introduce HistoryMetadataBuilder class in SemanticKernelPersonalized.History namespace. This class includes static methods MessageContent and ItemContent to generate metadata dictionaries for messages and items. MessageContent allows for additional metadata and optional parameters like source and name, while ItemContent provides a simpler dictionary with a default author value of "Unknown". Added necessary using directives.

---
+++ History/HistoryMetadataBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemanticKernelPersonalized.History
+{
+    public class HistoryMetadataBuilder
+    {
+        public static IReadOnlyDictionary<string, object?> MessageContent(
+            IDialog dialog,
+            string author,
+            Dictionary<string, object>? additionalMetaData = null,
+            string? source = null,
+            string? name = null
+            )
+        {
+            var metadata = new Dictionary<string, object?>
+            {
+                { "uuid", Guid.NewGuid().ToString() },
+                { "conversation_uuid", dialog.GetUuid().ToString() },
+                { "modelId", dialog.GetModelId() },
+                { "createAt", DateTime.Now.ToString() },
+                { "source", source },
+                { "name", name },
+                { "author", author }
+            };
+
+            if (additionalMetaData != null) metadata.Concat(additionalMetaData).ToDictionary(x => x.Key, x => x.Value);
+
+            return metadata;
+        }
+
+        public static IReadOnlyDictionary<string, object?> ItemContent(
+            IDialog dialog,
+            string author = "Unknown"
+            )
+        {
+            var metadata = new Dictionary<string, object?>
+            {
+                { "uuid", Guid.NewGuid().ToString() },
+                { "conversation_uuid", dialog.GetUuid().ToString() },
+            };
+
+            return metadata;
+        }
+    }
+}
