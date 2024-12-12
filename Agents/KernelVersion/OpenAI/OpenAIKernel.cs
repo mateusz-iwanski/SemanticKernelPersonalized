2da3a378e2f3584731d2e64ab46f8e62abc00425
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-12 13:32:22 +0100

    Refactor OpenAIKernel and enhance configuration
    Refactored the `OpenAIKernel` class to improve initialization and configuration. Made `_kernel` field non-readonly. Added private readonly fields `_openAISettings` and `_firecrawlSemanticSettings`. Introduced `_settings` property with a default value. Added a new constructor for initializing settings and default model settings. Replaced `GetResponseAsync` with new methods: `CustomizeModel`, `ConfigureSettings`, `InvokeAsync`, and `GetModelId`. Enhanced `InvokeAsync` with prompt configuration and execution logic. Added comments and XML documentation for better maintainability.

--- Agents/KernelVersion/OpenAI/OpenAIKernel.cs
+++ Agents/KernelVersion/OpenAI/OpenAIKernel.cs
@@ -10,6 +10,7 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Services;
+using OpenAI.Models;
 using SemanticKernelPersonalized.Plugins.WebScrapping;
 using SemanticKernelPersonalized.Settings;
 
@@ -19,64 +20,94 @@ namespace SemanticKernelPersonalized.Agents.KernelVersion.OpenAi
 {
     public class OpenAIKernel : IConnectorKernel
     {
-        private readonly Kernel _kernel;
+        private Kernel _kernel;
+        private readonly IOptions<OpenAISemanticSettings> _openAISettings;
+        private readonly IOptions<FirecrawlSemanticSettings> _firecrawlSemanticSettings;
 
+        private PromptExecutionSettings _settings { get; set; } = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenAIKernel"/> class with default settings.
+        /// If you want to change the default model ID settings, use the <see cref="CustomizeModel"/> method 
+        /// If you want to change the default kernel settings, use the <see cref="SetModelSettings"/> method.
+        /// </summary>
+        /// <param name="openAISettings">The settings for OpenAI, including API key, organization ID, service ID, and default model ID.</param>
+        /// <param name="firecrawlSemanticSettings">The settings for Firecrawl, including API key, API URL, and API map URL.</param>
+        /// <remarks>
+        /// This constructor initializes the kernel with chat completion capabilities using the provided OpenAI and Firecrawl settings.
+        /// It also sets the default model settings for the OpenAIChatCompletionService:
+        ///     - FunctionChoiceBehavior: Auto()
+        /// </remarks>
         public OpenAIKernel(
             IOptions<OpenAISemanticSettings> openAISettings, 
             IOptions<FirecrawlSemanticSettings> firecrawlSemanticSettings
             )
         {
-            _kernel = new KernelBuilder().CreateKernelWithChatCompletion(
-                openAISettings,
-                firecrawlSemanticSettings
+            _openAISettings = openAISettings;
+            _firecrawlSemanticSettings = firecrawlSemanticSettings;
+
+            _kernel = new KernelBuilder().CreateKernelWithOpenAIChatCompletion(
+                modelId: _openAISettings.Value.DefaultModelId,
+                modelApiKey: _openAISettings.Value.ApiKey,
+                firecrawlSemanticSettings: _firecrawlSemanticSettings
                 );
         }
-        public async Task<string> GetResponseAsync(string input)
+
+        public void CustomizeModel(string modelId)
         {
-            //IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
-            //kernelBuilder.AddOpenAIChatCompletion(
-            //    modelId: "gpt-4",
-            //    apiKey: _openAisettings.ApiKey,
-            //    orgId: "", // Optional
-            //    serviceId: "", // Optional; for targeting specific services within Semantic Kernel
-            //    httpClient: new HttpClient() // Optional; if not provided, the HttpClient from the kernel will be used
-            //);
-            //Kernel kernel = kernelBuilder.Build();
+            _kernel = new KernelBuilder().CreateKernelWithOpenAIChatCompletion(
+                modelId: _openAISettings.Value.DefaultModelId,
+                modelApiKey: _openAISettings.Value.ApiKey,
+                firecrawlSemanticSettings: _firecrawlSemanticSettings
+                );
+        }
 
-            //kernel.Plugins.AddFromType<IWebScraping>("WebScrapping");
+        /// <summary>
+        /// Set the kernel settings for the OpenAIChatCompletionService.
+        /// </summary>
+        /// <remarks>
+        /// Default settings are created in the constructor
+        /// </remarks>
+        /// <param name="functionChoiceBehavior"></param>
+        public void ConfigureSettings(
+            FunctionChoiceBehavior functionChoiceBehavior
+            )
+        {
+            _settings = new() { 
+                FunctionChoiceBehavior = functionChoiceBehavior, 
+            };
+        }
 
-            //var kernel = new KernelBuilder().CreateKernelWithChatCompletion(_openAisettings, _firecrawlSemanticSettings);
+        /// <summary>
+        /// Invokes the kernel with the provided prompt and returns the result as a string.
+        /// </summary>
+        /// <param name="prompt">The prompt to be processed by the kernel.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the response from the kernel as a string.</returns>
+        public async Task<string> InvokeAsync(string prompt)
+        {
+            // Add for logging in Azure Application Insights -> Dependency -> Dependecy (name)
+            // I use it for tracking the request
+            // It will appear in the Application Insights logs, in the first dependency thread
+            var promptConfig = JsonSerializer.Deserialize<PromptTemplateConfig>(
+                """
+                { "name" : "MapScrapper" }
+                """
+                )!;
 
-            // Enable planning
-            PromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+            promptConfig.Template = prompt;
 
-            string configPayload = """
-            {
-                "schema": 1,
-                "name": "HelloAI",
-                "description": "Say hello to an AI",
-                "type": "completion",
-                "completion": {
-                "max_tokens": 256,
-                "temperature": 0.5,
-                "top_p": 0.0,
-                "presence_penalty": 0.0,
-                "frequency_penalty": 0.0
-                }
-            }
-            """;
-            //var promptConfig = JsonSerializer.Deserialize<PromptTemplateConfig>(configPayload)!;
+            promptConfig.AddExecutionSettings(_settings);
 
-            //promptConfig.Template = "pobierz url z www.rtk.pl";
-            //var func = kernel.CreateFunctionFromPrompt(promptConfig);
-            //var result = await kernel.InvokeAsync(func);
-
-            var result = await kernel.InvokePromptAsync("pobierz url z www.rtk.pl", new(settings));
-            Console.WriteLine(result.GetValue<string>());
+            var func = _kernel.CreateFunctionFromPrompt(promptConfig);
+            var result = await _kernel.InvokeAsync(func);
 
             return result.GetValue<string>();
         }
 
+        /// <summary>
+        /// Get the model ID of the current kernel.
+        /// </summary>
+        /// <returns>Model ID</returns>
         public string? GetModelId()
         {
             var chatCompletionService = _kernel.GetRequiredService<IChatCompletionService>();
