fd773d0ba02fd29b9855ae51d4dcf6e9a4bb5209
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-12 10:56:22 +0100

    Add IConnectorStandalone interface and new namespace
    Introduced a new namespace `SemanticKernelPersonalized.Agents.StandalobeVersion` and defined the `IConnectorStandalone` interface within it. This interface includes methods `GetChatCompletionService()` and `GetModelId()`. Added necessary using directives for `Microsoft.SemanticKernel.ChatCompletion` and `Microsoft.SemanticKernel.Connectors.OpenAI`.

---
+++ Agents/StandalobeVersion/IConnectorStandalone.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace SemanticKernelPersonalized.Agents.StandalobeVersion
+{
+    public interface IConnectorStandalone
+    {
+        IChatCompletionService GetChatCompletionService();
+        string GetModelId();
+    }
+}
