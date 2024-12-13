bc1c38d844bc70c498fbbebb02adf432d19d73c4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-13 17:00:01 +0100

    Refactor properties and add validation checks
    Changed ApiKey and DefaultModelId to private fields with public accessors, adding ArgumentNullException checks. Added _settingsMap field for exception messages. Added using System.Threading.Tasks.

--- Settings/OpenAISemanticSettings.cs
+++ Settings/OpenAISemanticSettings.cs
@@ -8,10 +8,37 @@ namespace SemanticKernelPersonalized.Settings
 {
     public class OpenAISemanticSettings
     {
-        public string ApiKey { get; set; }
+        private string _settingsMap { get; set; } = "SemanticKernel->Access->OpenAi";
+        // It must be in the settings.json file
+        private string _apiKey { get; set; }
+
+        // It must be in the settings.json file
+        private string _defaultModelId { get; set; }
+
+        public string ApiKey 
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_apiKey)) throw new ArgumentNullException($"{_settingsMap}->{nameof(ApiKey)} - must be in your settings.json");
+                return _apiKey;
+            }
+            set => _apiKey = value;
+        }
+
+        public string DefaultModelId 
+        { 
+            get
+            {
+                if (string.IsNullOrEmpty(_defaultModelId)) 
+                    throw new ArgumentNullException($"{_settingsMap}->{nameof(DefaultModelId)} - must be in your settings.json");
+                return _defaultModelId;
+            }
+            set => _defaultModelId = value;
+        }
+
         public string OrganizationId { get; set; }
         public string ServiceId { get; set; }
-        public string DefaultModelId { get; set; }
+        
 
     }
 }
