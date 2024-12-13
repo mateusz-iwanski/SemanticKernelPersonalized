c29db14670431acbd23e921af4cb323233299510
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-13 16:56:38 +0100

    Refactor OpenAiStandalone and enhance configurability
    Replaced `using` directive for `SemanticKernelPersonalized.Agents.StandalobeVersion` with `SemanticKernelPersonalized.Agents.KernelVersion`. Updated `OpenAiStandalone` to implement `IConnector` instead of `IConnectorStandalone`. Added summary comment for `OpenAiStandalone` agent. Introduced `_settings` property of type `OpenAIPromptExecutionSettings`. Enhanced constructor with detailed parameter descriptions and removed validation checks for `DefaultModelId` and `ApiKey`. Added `CustomizeModel` method and replaced `ChangeModelId` with `ConfigureSettings` for comprehensive AI model execution settings. Added methods `GetPromptExecutionSettings`, `GetChatCompletionService`, and `GetKernel`.

--- Agents/StandalobeVersion/OpenAI/OpenAiStandalone.cs
+++ Agents/StandalobeVersion/OpenAI/OpenAiStandalone.cs
@@ -14,65 +14,106 @@ using Microsoft.SemanticKernel.Services;
 using SemanticKernelPersonalized.Settings;
 using Microsoft.Extensions.AI;
 using System.Configuration;
-using SemanticKernelPersonalized.Agents.StandalobeVersion;
+using SemanticKernelPersonalized.Agents.KernelVersion;
 
 namespace SemanticKernelPersonalized.Agents.Standalone.OpenAi
 {
-    public class OpenAiStandalone : IConnectorStandalone, IImageAnalyzer
+
+    /// <summary>
+    /// Standalone agent doesn't have a kernel, so it uses the OpenAIChatCompletionService directly.
+    /// OpenAiStandalone agent doesn't use plugins.
+    /// </summary>
+    public class OpenAiStandalone : IConnector, IImageAnalyzer
     {
         private readonly ILogger<OpenAiStandalone> _logger;
         private readonly OpenAISemanticSettings _openAISettings;
         private OpenAIChatCompletionService _chatCompletionService { get; set; }
+        private OpenAIPromptExecutionSettings _settings { get; set; } = new() { };
 
         /// <summary>
-        /// Create a new instance of the OpenAIChatCompletionService with default model id.
-        /// Look on settings JSON file and check what default model id is set.
+        /// Initializes a new instance of the <see cref="OpenAiStandalone"/> class with default settings.
+        /// If you want to change the default model ID settings, use the <see cref="CustomizeModel"/> method 
+        /// If you want to change the default kernel settings, use the <see cref="SetModelSettings"/> method.
         /// </summary>
-        /// <param name="openAISettings"></param>
-        /// <param name="logger"></param>
+        /// <param name="openAISettings">The settings for OpenAI, including API key, organization ID, service ID, and default model ID.</param>
+        /// <remarks>
+        /// In the constructor set the default model settings for the OpenAIChatCompletionService:
+        /// </remarks>
         public OpenAiStandalone(IOptions<OpenAISemanticSettings> openAISettings, ILogger<OpenAiStandalone> logger)
         {
             _openAISettings = openAISettings.Value;
 
             _logger = logger;
 
-            if (
-                string.IsNullOrEmpty(_openAISettings.DefaultModelId) ||
-                string.IsNullOrEmpty(_openAISettings.ApiKey)
-                )
-            {
-                throw new Exception("Your settings file doesn't have OpenAiStandalone data.");
-            }
-
             _chatCompletionService = new OpenAIChatCompletionService(
                 modelId: _openAISettings.DefaultModelId,
                 apiKey: _openAISettings.ApiKey,
-                organization: _openAISettings.OrganizationId  // Optional
-                                                              //loggerFactory: logger  
-                                                              // TODO: Add logger factory in DI
+                organization: _openAISettings.OrganizationId  
             );
+        }
+        public void CustomizeModel(string modelId)
+        {
+            _chatCompletionService = new OpenAIChatCompletionService(
+                modelId: modelId,
+                apiKey: _openAISettings.ApiKey,
+                organization: _openAISettings.OrganizationId);
         }
 
         /// <summary>
-        /// Changes the model ID for the OpenAIChatCompletionService.
+        /// Configures the AI model execution settings with specified parameters.
         /// </summary>
-        /// <param name="newModelId">The new model ID to use.</param>
-        public void ChangeModelId(string newModelId)
+        /// <param name="functionChoiceBehavior">Determines how the AI model should choose and execute functions. In standalone agent it won't work, it doesn't use plugins. </param>
+        /// <param name="maxTokens">Maximum number of tokens to generate in the response.</param>
+        /// <param name="temperature">Controls randomness in the response. Values range from 0 to 1, where higher values produce more random output.</param>
+        /// <param name="topP">Controls diversity via nucleus sampling. Values range from 0 to 1, providing finer control over output randomness.</param>
+        /// <param name="frequencyPenalty">Reduces repetition by penalizing frequent tokens. Values typically range from -2.0 to 2.0, where positive values decrease the likelihood of repeated information.</param>
+        /// <param name="presencePenalty">Reduces repetition by penalizing tokens that have appeared at all. Values typically range from -2.0 to 2.0, where positive values decrease the likelihood of using any token that has appeared before.</param>
+        /// <param name="stopSequences">List of sequences that will cause the text generation to stop when encountered.</param>
+        /// <param name="tokenSelectionBiases">Dictionary to modify likelihood of specific tokens. Keys are token IDs and values are bias values where positive values increase likelihood, negative values decrease it.</param>
+        public void ConfigureSettings(
+            FunctionChoiceBehavior functionChoiceBehavior,
+            int maxTokens,
+            double temperature,
+            double topP,
+            double frequencyPenalty,
+            double presencePenalty,
+            List<string> stopSequences,
+            Dictionary<int, int> tokenSelectionBiases
+        )
         {
-            _chatCompletionService = new OpenAIChatCompletionService(
-                modelId: newModelId,
-                apiKey: _openAISettings.ApiKey,
-                organization: _openAISettings.OrganizationId // Optional
+            _settings = new()
+            {
+                FunctionChoiceBehavior = functionChoiceBehavior,
 
-            //loggerFactory: logger
-            );
+                // Response length and randomness
+                MaxTokens = maxTokens,
+                Temperature = temperature,
+                TopP = topP,
 
-            _logger.LogInformation($"Changed modelId for OpenAI to: {newModelId}");
+                // Token selection
+                FrequencyPenalty = frequencyPenalty,
+                PresencePenalty = presencePenalty,
+
+                // Stop sequences
+                StopSequences = stopSequences, //new List<string> { "STOP", "\n" },
+
+                // Token counting
+                TokenSelectionBiases = tokenSelectionBiases
+                //new Dictionary<int, int>
+                //{
+                //    { 123, 10 },  // Increase likelihood of token 123
+                //    { 456, -10 }  // Decrease likelihood of token 456
+                //}
+            };
         }
+
+        public OpenAIPromptExecutionSettings GetPromptExecutionSettings() => _settings;
 
         public IChatCompletionService GetChatCompletionService() => _chatCompletionService;
 
         public string GetModelId() => _chatCompletionService.GetModelId();
 
+        // Standalone version doesn't have a kernel
+        public Kernel? GetKernel() => null;
     }
 }
