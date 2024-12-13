4c58ce7b1c4f51d7bfd054ccd272be45edec56a1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-13 16:59:50 +0100

    Refactor properties with validation in settings class
    Refactored `FirecrawlSemanticSettings.cs` to include validation logic for `ApiKey`, `ApiUrl`, and `ApiMapUrl` properties. Replaced original public properties with private backing fields and added new public properties that throw `ArgumentNullException` if the values are null or empty. Introduced a private field `_settingsMap` to store the settings path for error messages.

--- Settings/FirecrawlSemanticSettings.cs
+++ Settings/FirecrawlSemanticSettings.cs
@@ -8,8 +8,46 @@ namespace SemanticKernelPersonalized.Settings
 {
     public class FirecrawlSemanticSettings
     {
-        public string ApiKey { get; set; }
-        public string ApiUrl { get; set; }
-        public string ApiMapUrl { get; set; }
+        private string _settingsMap { get; set; } = "SemanticKernel->Access->Firecrawl";
+
+        private string _apiKey { get; set; }
+        private string _apiUrl { get; set; }
+        private string _apiMapUrl { get; set; }
+
+        // It must be in the settings.json file
+        public string ApiKey 
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_apiKey)) 
+                    throw new ArgumentNullException($"{_settingsMap}->{nameof(ApiKey)} - must be in your settings.json");
+                return _apiKey;
+            }
+            set => _apiKey = value;
+        }
+
+        // It must be in the settings.json file
+        public string ApiUrl 
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_apiUrl))
+                    throw new ArgumentNullException($"{_settingsMap}->{nameof(ApiUrl)} - must be in your settings.json");
+                return _apiUrl;
+            }
+            set => _apiUrl = value;
+        }
+
+        // It must be in the settings.json file
+        public string ApiMapUrl 
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_apiMapUrl))
+                    throw new ArgumentNullException($"{_settingsMap}->{nameof(ApiMapUrl)} - must be in your settings.json");
+                return _apiMapUrl;
+            }
+            set => _apiMapUrl = value;
+        }
     }
 }
