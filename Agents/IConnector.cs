61022b64422c8717246a6be04e3035be3b805c2e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-13 16:56:50 +0100

    Add IConnector interface and necessary using directives
    Introduced a new interface `IConnector` in the `SemanticKernelPersonalized.Agents` namespace. This interface includes methods for chat completion services, model customization, and configuration settings. Added using directives for `Microsoft.SemanticKernel` and related namespaces.

---
+++ Agents/IConnector.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace SemanticKernelPersonalized.Agents
+{
+    public interface IConnector
+    {
+        public IChatCompletionService GetChatCompletionService();
+        public void CustomizeModel(string modelId);
+        public string GetModelId();
+        public Kernel? GetKernel();
+        public void ConfigureSettings(
+            FunctionChoiceBehavior functionChoiceBehavior,
+            int maxTokens,
+            double temperature,
+            double topP,
+            double frequencyPenalty,
+            double presencePenalty,
+            List<string> stopSequences,
+            Dictionary<int, int> tokenSelectionBiases
+        );
+        public OpenAIPromptExecutionSettings GetPromptExecutionSettings();
+    }
+}
