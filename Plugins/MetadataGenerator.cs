42bc4bde688d19d8341ae8810eeef8f00e2f6785
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-12-21 21:46:30 +0100

    Add MetadataGenerator class and initialize Kernel instance
    Introduced the `MetadataGenerator` class within the new `SemanticKernelPersonalized.Plugins` namespace. Added necessary using directives for various namespaces. Implemented a constructor to initialize a private readonly `_kernel` field of type `Kernel`. Commented out the `GetChatHistory` method, which is intended for future implementation.

---
+++ Plugins/MetadataGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using SemanticKernelPersonalized.Agents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SemanticKernelPersonalized.Plugins
+{
+    public class MetadataGenerator
+    {
+        private readonly Kernel _kernel;
+
+        public MetadataGenerator(Kernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        //[KernelFunction("detail_fun")]
+        //[Description("Fun detail with data")]
+        //public string GetChatHistory()
+        //{
+        //    // how to get chathistory from chatCompletion?
+        //    //return  JsonSerializer.Serialize(connector.getConversationHistory().GetHistory());
+        //}
+
+
+    }
+}
